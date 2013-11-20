a47baa4cdb62652cc20d31dcb13f1a151b64e97c
Bud Byrd <bud.byrd@rackspace.com>
2013-11-19 19:26:40 -0600

    Initial commit

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,23 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+
+grails.project.dependency.resolution = {
+    inherits("global")
+    log "warn"
+
+    repositories {
+        grailsCentral()
+        mavenCentral()
+    }
+
+    plugins {
+        build(":tomcat:$grailsVersion",
+              ":release:2.0.3",
+              ":rest-client-builder:1.0.2") {
+            export = false
+        }
+
+        compile ':jaxrs:0.6'
+    }
+}
